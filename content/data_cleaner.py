e58d53dcb2b4dbe6d85013c4d5eb36f768e6f2c8
Nathanael Mariaule <nathanael.mariaule@gmail.com>
2021-06-02 15:07:59 +0200

    comments

--- content/data_cleaner.py
+++ content/data_cleaner.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 
 
-def eliza_cleaning(datas):
+def eliza_cleaning(datas: pd.DataFrame)->pd.DataFrame:
+    """
+    the function take for input the dataset https://raw.githubusercontent.com/JulienAlardot/challenge-collecting-data/main/Data/database.csv'
+    it changes the column names and remove useless datas
+    :param datas: pandas DataFrame that contains the dataset
+    :return datas: pandas DataFrame that contains the dataset after cleaning
+    """
     datas.drop(columns=['Unnamed: 0'], inplace=True)
     old_col=(datas.columns)
     new_col=[]
@@ -11,15 +17,15 @@ def eliza_cleaning(datas):
         item =item.lower()
         item= item.replace(' ','_')
         new_col.append(item)
-
     datas.columns = new_col
-    datas = datas[~datas['price'].isnull()]
+    #keep the usefull columns
     columns_restricted = ['locality', 'type_of_property', 'subtype_of_property',
            'price', 'type_of_sale', 'number_of_rooms', 'area',
            'fully_equipped_kitchen', 'furnished', 'open_fire', 'terrace',
            'terrace_area', 'garden', 'garden_area', 'surface_of_the_land',
            'surface_area_of_the_plot_of_land', 'number_of_facades',
            'swimming_pool', 'state_of_the_building'] # columns name without url and source
+    #change columns type
     datas = datas[~datas[columns_restricted].duplicated()]
     datas = datas.astype({'locality':'Int64', 
             'price':'float64', 
@@ -37,30 +43,35 @@ def eliza_cleaning(datas):
             'number_of_facades':'Int64',
             'swimming_pool':'Int64',
             })
+    # drop datas with nan price
     datas = datas[~datas['price'].isnull()]
     datas.drop(columns =['source', 'url'], inplace=True)
+    #move price columns at first place
     prices = datas.pop('price')
     datas.insert(0, 'price', prices)
+    #move type_of_property columns last place
     type_of_property = datas.pop('type_of_property')
     datas['type_of_property'] = type_of_property
-    datas = datas[((datas['price']<3*10**7) & (datas['price']>2500)) | (datas.price.isnull())]
-    #datas = datas[(datas['number_of_rooms']<100) | (datas.number_of_rooms.isnull())]
-    #datas = datas[(datas['area']<755000) | (datas.area.isnull())]
-    #datas = datas[(datas['terrace_area']<3000) | (datas.terrace_area.isnull())]
-    #datas = datas[(datas['garden_area']<100000) | (datas.garden_area.isnull())]
-    #datas = datas[(datas['surface_of_the_land']<100000) | (datas.surface_of_the_land.isnull())]
-    #datas = datas[(datas['number_of_facades']<5) | (datas.number_of_facades.isnull())]
+    #drop useless columns
     datas.drop(columns=['surface_area_of_the_plot_of_land', 'type_of_sale', 'subtype_of_property'], inplace=True)
-    datas['garden_area'] = datas['garden_area'].where(datas['garden'] == 1, other=datas['garden'])
-    datas['terrace_area'] = datas['terrace_area'].where(datas['terrace'] == 1, other=datas['terrace'])
     return datas
 
-
-def eliza_fillna(datas):
+def eliza_fillna(datas: pd.DataFrame)->pd.DataFrame:
+    """
+        the function take for input the dataset https://raw.githubusercontent.com/JulienAlardot/challenge-collecting-data/main/Data/database.csv'
+        it fill some nan values in the dataset
+        :param datas: pandas DataFrame that contains the dataset
+        :return datas: pandas DataFrame that contains the dataset after filling some nan's value
+    """
+    # if garden=0, we set garden_area as 0 same for terrace
+    datas['garden_area'] = datas['garden_area'].where(datas['garden'] == 1, other=datas['garden'])
+    datas['terrace_area'] = datas['terrace_area'].where(datas['terrace'] == 1, other=datas['terrace'])
+    # add new category for nan value in state_of_the_building and type_of_property
     datas['state_of_the_building'].fillna(value='unkown', inplace=True)
     datas['type_of_property'].fillna(value='other', inplace=True)
     zeros = datas.fillna(value=0)
     mask = (datas.type_of_property=='apartment')  & (datas.surface_of_the_land.isnull()) & (datas.garden_area==0)
+    #an appartement with no garden has 0 surface_of_the_land
     datas['surface_of_the_land'] = zeros.where(mask, other=datas).surface_of_the_land
     datas['furnished'].fillna(value=0, inplace=True)
     return datas
--- content/model_training.py
+++ content/model_training.py
@@ -20,80 +20,106 @@ from sklearn.preprocessing import MinMaxScaler
 from pickle import dump
 
 
+def model_training()->None:
+    """
+    create and train a xgboost model on the dataset
+    'https://raw.githubusercontent.com/JulienAlardot/challenge-collecting-data/main/Data/database.csv'
+    to predict housing's price in belgium
+    """
+    #loading and preliminary cleaning of the data
+    raw_datas = pd.read_csv(
+    'https://raw.githubusercontent.com/JulienAlardot/challenge-collecting-data/main/Data/database.csv')
+    datas = eliza_cleaning(raw_datas)
+    datas = eliza_fillna(datas)
+    #add a column with the median price for houses in the commune
+    datas = add_median_price(datas)
 
-raw_datas = pd.read_csv('https://raw.githubusercontent.com/JulienAlardot/challenge-collecting-data/main/Data/database.csv')
-datas = eliza_cleaning(raw_datas)
-datas = eliza_fillna(datas)
+    y = datas.pop('price')
+    X = datas
 
-median = pd.read_csv('median.csv')
-post = pd.read_csv('post_codes.csv', sep=';')
-median['Gemeente'] = median['Gemeente'].str.lower()
-post['Commune Principale'] = post['Commune principale'].str.lower()
-median_with_post = median.merge(post[['Code postal', 'Commune Principale']], how='left', left_on='Gemeente', right_on='Commune Principale')
-median_with_post = median_with_post.groupby('Gemeente').median()
-median_with_post['Mediaanprijs 2020'].fillna(median_with_post['Mediaanprijs 2019'], inplace=True)
-median_with_post['Mediaanprijs 2020'].fillna(median_with_post['Mediaanprijs 2018'], inplace=True)
-median_with_post.sort_values(by='Code postal', inplace=True)
-median_with_post.fillna(method='bfill', inplace=True)
-median_with_post.reset_index(inplace=True)
-median = median.merge(median_with_post[['Gemeente', 'Mediaanprijs 2020']], on='Gemeente')
-median_with_post = median.merge(post[['Code postal', 'Commune Principale']], how='left', left_on='Gemeente', right_on='Commune Principale')
-median_prices = median_with_post[['Code postal', 'Mediaanprijs 2020_y']]
-median_prices.columns = ['postal_code', 'median_price']
-median_prices = median_prices.groupby('postal_code').mean()
-median_prices.reset_index(inplace=True)
-median_prices['postal_code'] = median_prices['postal_code'].astype('int64')
-datas = datas.merge(median_prices, how='left', left_on='locality', right_on='postal_code')
-datas.drop('postal_code', inplace=True, axis=1)
-datas.sort_values(by='locality', ascending=False, inplace=True)
-datas['median_price'].fillna(method='ffill', inplace=True)
-datas.sort_index(inplace=True)
-col = datas.columns
-col = [col[0]]+[col[-1]]+list(col[1:-1])
-datas = datas[col]
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
 
-y = datas.pop('price')
-X = datas
-
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
-
-
-fillna = ColumnTransformer(
-        [('imp_col1', SimpleImputer(strategy='mean'), ['area', 'terrace_area', 'garden_area', 
-                                                      'surface_of_the_land']),
-         ('imp_col2', SimpleImputer(strategy='median'), ['number_of_rooms', 'number_of_facades']),
-        ],remainder='passthrough')
-
-enc = ColumnTransformer(
-        [
-         ('enc', OneHotEncoder(sparse = False, drop ='first'), [-4, -3,-2,-1]),
+    #preprocessing
+    fillna = ColumnTransformer(
+        [('imp_col1', SimpleImputer(strategy='mean'), ['area', 'terrace_area', 'garden_area',
+                                                   'surface_of_the_land']),
+        ('imp_col2', SimpleImputer(strategy='median'), ['number_of_rooms', 'number_of_facades']),
         ], remainder='passthrough')
 
+    enc = ColumnTransformer(
+        [
+            ('enc', OneHotEncoder(sparse=False, drop='first'), [-4, -3, -2, -1]),
+        ], remainder='passthrough')
 
-pipe = make_pipeline(fillna, enc, MinMaxScaler(), StandardScaler())
-pipe.fit(X)
-X_train = pipe.transform(X_train)
-X_val = pipe.transform(X_val)
+    pipe = make_pipeline(fillna, enc, MinMaxScaler(), StandardScaler())
+    pipe.fit(X)
+    X_train = pipe.transform(X_train)
+    X_val = pipe.transform(X_val)
+
+    #creation and training of the model
+    my_XGB_model = XGBRegressor(n_estimators=3000, max_depth=11, min_child_weight=11,
+                                learning_rate=0.01, subsample=1, colsample_bytree=1,
+                                eval_metric='mae')
+    my_XGB_model.fit(X_train, np.log(y_train), early_stopping_rounds=10,
+                     eval_set=[(X_val, np.log(y_val))], verbose=False)
+
+    X = pipe.transform(X)
+    #print performances of the model
+    XGB_predictions = my_XGB_model.predict(X)
+    XGB_predictions = np.exp(XGB_predictions)
+    XGB_mult_mae = mean_absolute_error(XGB_predictions, y)
+    print("Validation MAE for multi-pass XGBoost Model : " + str(XGB_mult_mae))
+    preds = my_XGB_model.predict(X)
+    rmse = np.sqrt(mean_squared_error(y, np.exp(preds)))
+    print("RMSE: %f" % (rmse))
+    print("R2 score: ", my_XGB_model.score(X, np.log(y)))
+
+    #save the preprocessing pipeline and the model
+    dump(pipe, open('preprocessor.pkl', 'wb'))
+    my_XGB_model.save_model("model.json")
 
 
+def add_median_price(datas: pd.DataFrame)->pd.DataFrame:
+    """
+        Take for input a dataframe with a column 'postal_code' that contains belgian postal code
+        and add a column with the median price of the house in the corresponding commune
+        :param datas: a pd.DataFrame with a column 'postal_code'
+        :return datas: the pd.DataFrame with a column media_price added
+    """
+    #open csv with the required datas
+    median = pd.read_csv('median.csv')
+    post = pd.read_csv('post_codes.csv', sep=';')
+    #preprocessing of the data
+    median['Gemeente'] = median['Gemeente'].str.lower()
+    post['Commune Principale'] = post['Commune principale'].str.lower()
+    median_with_post = median.merge(post[['Code postal', 'Commune Principale']], how='left', left_on='Gemeente',
+                                    right_on='Commune Principale')
+    median_with_post = median_with_post.groupby('Gemeente').median()
+    median_with_post['Mediaanprijs 2020'].fillna(median_with_post['Mediaanprijs 2019'], inplace=True)
+    median_with_post['Mediaanprijs 2020'].fillna(median_with_post['Mediaanprijs 2018'], inplace=True)
+    median_with_post.sort_values(by='Code postal', inplace=True)
+    median_with_post.fillna(method='bfill', inplace=True)
+    median_with_post.reset_index(inplace=True)
+    #merge the datas
+    median = median.merge(median_with_post[['Gemeente', 'Mediaanprijs 2020']], on='Gemeente')
+    median_with_post = median.merge(post[['Code postal', 'Commune Principale']], how='left', left_on='Gemeente',
+                                    right_on='Commune Principale')
+    median_prices = median_with_post[['Code postal', 'Mediaanprijs 2020_y']]
+    median_prices.columns = ['postal_code', 'median_price']
+    median_prices = median_prices.groupby('postal_code').mean()
+    median_prices.reset_index(inplace=True)
+    median_prices['postal_code'] = median_prices['postal_code'].astype('int64')
+    datas = datas.merge(median_prices, how='left', left_on='locality', right_on='postal_code')
+    datas.drop('postal_code', inplace=True, axis=1)
+    #autofill remaing nan values in the final dataset
+    datas.sort_values(by='locality', ascending=False, inplace=True)
+    datas['median_price'].fillna(method='ffill', inplace=True)
+    datas.sort_index(inplace=True)
+    col = datas.columns
+    col = [col[0]] + [col[-1]] + list(col[1:-1])
+    datas = datas[col]
+    return datas
 
-my_XGB_model = XGBRegressor(n_estimators=3000, max_depth=11, min_child_weight=11,
-                           learning_rate=0.01, subsample=1, colsample_bytree=1,
-                           eval_metric='mae')
-my_XGB_model.fit(X_train, np.log(y_train), early_stopping_rounds=10, 
-             eval_set=[(X_val, np.log(y_val))], verbose=False)
+if __name__=='__main__':
+    model_training()
 
-X=pipe.transform(X)  
-XGB_predictions = my_XGB_model.predict(X)
-XGB_predictions = np.exp(XGB_predictions)
-XGB_mult_mae = mean_absolute_error(XGB_predictions, y)
-print("Validation MAE for multi-pass XGBoost Model : " + str(XGB_mult_mae))
-              
-  
-preds = my_XGB_model.predict(X)
-rmse = np.sqrt(mean_squared_error( y, np.exp(preds)))
-print("RMSE: %f" % (rmse))
-print("R2 score: ", my_XGB_model.score(X,  np.log(y)))
-
-dump(pipe, open('preprocessor.pkl', 'wb'))
-my_XGB_model.save_model("model.json")
